bd92d1cbec0da25f92a97d05dfa9446b2b207c7f
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-07 23:37:50 +0700

    slicing homepage

---
+++ src/pages/Home/Hero/index.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { FaChevronRight } from "react-icons/fa";
+
+import Button from "src/components/atoms/Button";
+
+import Iconimg from "src/assets/icon.png";
+import Heroimg from "src/assets/bg-home.jpg";
+
+const Hero = () => {
+  return (
+    <section
+      id="hero"
+      className="h-screen w-full flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: `linear-gradient(
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.5)
+    ), url(${Heroimg})`,
+        backgroundRepeat: `no-repeat`,
+        backgroundSize: `cover`,
+        backgroundPosition: `center`,
+      }}
+    >
+      <Link to={"/movies"}>
+        <img
+          src={Iconimg}
+          alt="icon"
+          className="hover:drop-shadow-[0_0_2em#469006ff] duration-300 w-52"
+        />
+      </Link>
+      <h1 className="mb-5 text-4xl md:text-6xl font-bold">Wazzup!</h1>
+      <p className="mb-5 text-sm md:text-md max-w-sm text-center">
+        Enjoy exclusive Cuthless Originals as well as popular movies and TV
+        shows. Watch now!
+      </p>
+      <Link to={"/movies"}>
+        <Button
+          label={
+            <span className="flex items-center gap-1">
+              Get Started <FaChevronRight />
+            </span>
+          }
+        />
+      </Link>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Hero from "src/pages/Home/Hero";
+import Footer from "src/components/molecules/Footer";
+import Header from "src/components/molecules/Header";
+import HomeContent from "src/components/molecules/HomeContent";
+
+import TVImg from "src/assets/tv.png";
+import DevicePileImg from "src/assets/device-pile.png";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <Header />
+      <Hero />
+      <HomeContent
+        id={`tv`}
+        title={`Enjoy on your TV`}
+        desc={`Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.`}
+        reverse={false}
+        frame={TVImg}
+        video={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v`}
+      />
+      <HomeContent
+        id={`device-pile`}
+        title={`Watch everywhere`}
+        desc={`Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.`}
+        reverse={true}
+        frame={DevicePileImg}
+        video={`https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-id.m4v`}
+      />
+      <Footer />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
