18c43466ff24451e1be317dce846c6b2a45e9023
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-08 11:02:51 +0700

    slicing search box component

--- src/components/molecules/SearchBox/index.jsx
+++ src/components/molecules/SearchBox/index.jsx
@@ -1,11 +1,58 @@
+import { Link } from "react-router-dom";
+import moment from "moment/moment";
+
 import Button from "src/components/atoms/Button";
 import Input from "src/components/atoms/Input";
 
-const SearchBox = () => {
+const SearchBox = ({
+  placeholder,
+  handleSearch,
+  isSearch = true,
+  data = [],
+  noData,
+}) => {
   return (
-    <div className="bg-wht rounded-md">
-      <Input />
-      <Button label={`Search`} classname={`rounded-md`} />
+    <div className="relative space-y-2 max-w-[270px]">
+      <div className="bg-wht rounded-md flex">
+        <Input placeholder={placeholder} onChange={handleSearch} />
+        <Button label={`Search`} classname={`rounded-md`} />
+      </div>
+      <div
+        className={`flex flex-col gap-2 rounded-md bg-wht text-blk p-2 w-full ${
+          isSearch ? `block` : `hidden`
+        }`}
+      >
+        {noData ? (
+          <p className="text-center text-xs italic text-slate-500">
+            Empty Search: No data matches found.
+          </p>
+        ) : (
+          data.slice(0, 4).map((data) => (
+            <Link
+              key={data.id}
+              to={`/movie/${data.id}`}
+              className="flex justify-between gap-2 w-full"
+            >
+              <img
+                src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${data.poster_path}`}
+                alt={data.title}
+                className="w-10"
+              />
+              <div className="w-[80%]">
+                <div className="flex justify-between whitespace-nowrap">
+                  <h6 className="font-semibold truncate max-w-[60%]">
+                    {data.title}
+                  </h6>
+                  <span>{moment(data.release_date).format("YYYY")}</span>
+                </div>
+                <p className="line-clamp-2 text-xs italic text-slate-500">
+                  {data.overview}
+                </p>
+              </div>
+            </Link>
+          ))
+        )}
+      </div>
     </div>
   );
 };
