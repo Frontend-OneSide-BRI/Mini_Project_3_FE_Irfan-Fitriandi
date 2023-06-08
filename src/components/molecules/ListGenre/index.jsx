8bb5292446e5d422672f91bb037a0127a70a28ae
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-08 19:44:45 +0700

    update logic in list genre for filtering data

--- src/components/molecules/ListGenre/index.jsx
+++ src/components/molecules/ListGenre/index.jsx
@@ -1,13 +1,16 @@
 import { categories } from "src/constants/ListCategories";
 
-const ListGenre = ({ handleFilter, isActive }) => {
+const ListGenre = ({ handleFilter, isActive, handlePage }) => {
   return (
     <nav>
       <ul className="flex justify-center items-center gap-2 md:gap-6">
         {categories.map((data) => (
           <li
             key={data.id}
-            onClick={() => handleFilter(data.id)}
+            onClick={() => {
+              handleFilter(data.id);
+              handlePage(1);
+            }}
             className={`text-lg font-bold px-[6px] whitespace-nowrap cursor-pointer transition-all duration-150 ease-in-out h-10 ${
               isActive === data.id ? `border-b-4 border-grn` : null
             }`}
