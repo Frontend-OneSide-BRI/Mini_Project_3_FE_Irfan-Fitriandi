d6840ad4abaa9d41baaed48c1327d1d92755fc5c
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-08 11:03:37 +0700

    update navigate in header component

--- src/components/molecules/Header/index.jsx
+++ src/components/molecules/Header/index.jsx
@@ -6,9 +6,11 @@ import Logo from "src/assets/logo.png";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center absolute w-full p-4 md:px-32 lg:px-44">
-      <img src={Logo} alt="logo" className="w-40" />
-      <Link >
+    <header className="flex justify-between items-center absolute w-full p-4 md:px-32 lg:px-44 z-10">
+      <Link to={`/`}>
+        <img src={Logo} alt="logo" className="w-40" />
+      </Link>
+      <Link>
         <Button label="Login" />
       </Link>
     </header>
