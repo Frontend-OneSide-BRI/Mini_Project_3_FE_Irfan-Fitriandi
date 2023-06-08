6bb3454c38c0f6643ffac743ac5da72b6e0f63a2
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-08 11:05:20 +0700

    update search section in movies page

--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import Header from "src/components/molecules/Header";
 
-import Heroimg from "src/assets/bg-movies.jpg";
-import SearchBox from "src/components/molecules/SearchBox";
 import ListCategory from "src/components/molecules/ListCategory";
-import { useState } from "react";
+import Loading from "src/components/atoms/Loading";
+import SearchHero from "./SearchHero";
 
 const MoviesPage = () => {
   const [category, setCategory] = useState(0);
@@ -11,22 +11,7 @@ const MoviesPage = () => {
   return (
     <>
       <Header />
-      <section
-        id="hero-movies"
-        className="h-[100vh] w-full flex flex-col justify-center items-center"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.96)), url(${Heroimg})`,
-          backgroundRepeat: `no-repeat`,
-          backgroundSize: `cover`,
-          backgroundPosition: `center`,
-        }}
-      >
-        <h1 className="text-2xl md:text-3xl font-bold text-center">Welcome.</h1>
-        <h3 className="mb-5 text-lg md:text-xl font-bold text-center">
-          Millions of movies, TV shows and people to discover. Explore now.
-        </h3>
-        <SearchBox />
-      </section>
+      <SearchHero />
       <ListCategory handleFilter={setCategory} isActive={category} />
     </>
   );
