0eb48032e6e5ec335c316b5b49ee3417fc205a39
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-08 23:54:02 +0700

    update header for auth button

--- src/components/molecules/Header/index.jsx
+++ src/components/molecules/Header/index.jsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
 
-import Button from "src/components/atoms/Button";
-
 import Logo from "src/assets/logo.png";
+import AuthButton from "../AuthButton";
 
-const Header = () => {
+const Header = ({ button = true }) => {
   return (
     <header className="flex justify-between items-center absolute w-full p-4 md:px-32 lg:px-44 z-10">
       <Link to={`/`}>
         <img src={Logo} alt="logo" className="w-40" />
       </Link>
-      <Link>
-        <Button label="Login" />
-      </Link>
+      {button ? <AuthButton /> : nul}
     </header>
   );
 };
