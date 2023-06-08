8aee4d0d36c5c198199ed134e3dc06d823708a4a
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-06-08 11:01:13 +0700

    setup react-redux and rtk query

--- src/services/apiReq/index.js
+++ src/services/apiReq/index.js
@@ -1,1 +1,16 @@
-//
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const movieApi = createApi({
+  reducerPath: "movieApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3/" }),
+  endpoints: (builder) => ({
+    getMovieByName: builder.query({
+      query: (query, page = 1) =>
+        `search/movie?api_key=${API_KEY}&query=${query}&include_adult=false&language=en-US&page=${page}`,
+    }),
+  }),
+});
+
+export const { useGetMovieByNameQuery } = movieApi;
